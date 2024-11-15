2fb1e1fefe1db55f235b2a84c0368723b7c1862a
r-gitrepo <16122224+r-gitrepo@users.noreply.github.com>
2024-11-14 17:40:19 -0800

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://wtrbqfilzokhzokazlfg.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0cmJxZmlsem9raHpva2F6bGZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjEyOTMwNDksImV4cCI6MTk3Njg2OTA0OX0.0Jg6KQmSajak2k6Qsa46sQfEShH5GaewCiOab0KmJzM',
+    url: 'https://.supabase.co',
+    anonKey: '',
   );
   runApp(MyApp());
 }
